4d0fbcc8400c990c5a442c94c1e51bea4fa26b4b
Alapan Chaudhuri <52317522+banrovegrie@users.noreply.github.com>
2020-09-10 03:41:32 +0530

    windows

--- ls.c
+++ ls.c
@@ -46,12 +46,12 @@ int LSL(char *directory, int A)
             printf("%c", (ST.st_mode & S_IRUSR) ? 'r' : '-');
             printf("%c", (ST.st_mode & S_IWUSR) ? 'w' : '-');
             printf("%c", (ST.st_mode & S_IXUSR) ? 'x' : '-');
-            printf("%1s", (ST.st_mode & S_IRGRP) ? 'r' : '-');
-            printf("%1s", (ST.st_mode & S_IWGRP) ? 'w' : '-');
-            printf("%1s", (ST.st_mode & S_IXGRP) ? 'x' : '-');
-            printf("%1s", (ST.st_mode & S_IROTH) ? 'r' : '-');
-            printf("%1s", (ST.st_mode & S_IWOTH) ? 'w' : '-');
-            printf("%1s ", (ST.st_mode & S_IXOTH) ? 'x' : '-');
+            printf("%c", (ST.st_mode & S_IRGRP) ? 'r' : '-');
+            printf("%c", (ST.st_mode & S_IWGRP) ? 'w' : '-');
+            printf("%c", (ST.st_mode & S_IXGRP) ? 'x' : '-');
+            printf("%c", (ST.st_mode & S_IROTH) ? 'r' : '-');
+            printf("%c", (ST.st_mode & S_IWOTH) ? 'w' : '-');
+            printf("%c ", (ST.st_mode & S_IXOTH) ? 'x' : '-');
 
             printf("%2ld ", (unsigned long) (ST.st_nlink));
 
@@ -76,7 +76,7 @@ int LSL(char *directory, int A)
 
 int LSMAIN(char *line)
 {
-    int index = 0, A = 0, L = 0;
+    int index = 0, A = 0, L = 0, count = 0;
 
     char *d = " \t\n";
     char *foo = strtok(line, d);
@@ -84,42 +84,81 @@ int LSMAIN(char *line)
 
     while (foo != NULL)
     {
+        printf("%s\n", foo);
         if (foo[0] == '-')
         {
             if (strcmp(foo, "-l") == 0) L = 1;
             else if (strcmp(foo, "-a") == 0) A = 1;
             else if (strcmp(foo, "-la") == 0) A = 1, L = 1;
             else if (strcmp(foo, "-al") == 0) A = 1, L = 1;
-        } else
-        {
-            str[index] = foo;
-            str[index][strlen(str[index])] = '\0';
-            index += 1;
+            count --;
         }
 
-        foo = strtok(NULL, d);
+        str[index] = foo, foo = strtok(NULL, d);
+        str[index][strlen(str[index])] = '\0';
+        index += 1;
+        count++;
     }
 
-    for (int i = 0; i < index; i++)
+    printf("L:%d A:%d index:%d\n", L, A, index);
+
+    if (count == 1)
     {
-        char foo[path_max];
-        strcpy(foo, home);
-
-        int l = strlen(home);
-        //printf("%s\n", home);
-        if (args[1] == NULL || strcmp(args[1], "\0") == 0) chdir(home);
-        else if (args[1][0] != '~')
-        {
-            if (chdir(args[1]) != 0) perror("error (a) in cd");
-        } else if (args[1][0] == '~')
-        {
-            int i = 0;
-            for (i = 1; args[1][i] != '\0'; i++)
-                foo[i + l - 1] = args[1][i];
-            foo[i + l - 1] = '\0';
-
-            if (chdir(foo) != 0) perror("error (b) in cd");
-        } else if (chdir(args[1]) != 0) perror("error (c) in cd");
-
+        if (L == 1) LSL(".", A);
+        else LS(".", A);
+        return 0;
     }
+
+    /*for (int i = 1; i < index; i++)
+    {
+        char *rel_path = malloc(path_max * sizeof(char));
+        if (getcwd(rel_path, path_max) == NULL) perror("error in cwd");
+
+        char *resolved_path = malloc(path_max * sizeof(char));
+
+        printf("%s\n", str[i]);
+
+        if (str[i][0] == '~')
+        {
+            strcpy(rel_path, home);
+            int l = strlen(home);
+
+            int ind = 0;
+            for (ind = 1; str[i][ind] != '\0'; ind++)
+                rel_path[ind + l - 1] = str[i][ind];
+            rel_path[ind + l - 1] = '\0';
+
+        } else if (str[i][0] == '/')
+        {
+            strcpy(rel_path, str[i]);
+            strcat(rel_path,"\0");
+        }
+        else if(str[i][0] == '\0' || str[i] == NULL)
+        {
+            //empty
+        }
+        else
+        {
+            strcat(rel_path, "/");
+            strcat(rel_path, str[i]);
+            strcat(rel_path, "\0");
+        }
+
+        printf("%s\n", rel_path);
+        if (realpath(rel_path, resolved_path) == NULL)
+        {
+            perror("error in locating directory");
+            return 0;
+        }
+        printf("%s\n", resolved_path);
+
+        if (L == 1) LSL(resolved_path, A);
+        else LS(resolved_path, A);
+
+        free(rel_path);
+        free(resolved_path);
+        rel_path = NULL, resolved_path = NULL;
+    }*/
+
+    return 0;
 }
