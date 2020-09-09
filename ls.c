40676a2d68a2371e094aae2dac728dfd31257ef4
Alapan Chaudhuri <52317522+banrovegrie@users.noreply.github.com>
2020-09-10 01:30:13 +0530

    From Windows

--- ls.c
+++ ls.c
@@ -1,5 +1,7 @@
 #include "others.h"
 
+extern char systemname[host_max], username[user_max], home[path_max], path[path_max];
+
 int LS(char *directory, int A)
 {
     struct dirent **LIST;
@@ -63,8 +65,7 @@ int LSL(char *directory, int A)
             printf("%s ", timer);
 
             printf("%s\n", LIST[i]->d_name);
-        }
-        else
+        } else
         {
             perror("error in permissions/details");
             return 0;
@@ -75,5 +76,50 @@ int LSL(char *directory, int A)
 
 int LSMAIN(char *line)
 {
-    
+    int index = 0, A = 0, L = 0;
+
+    char *d = " \t\n";
+    char *foo = strtok(line, d);
+    char *str[100] = {NULL};
+
+    while (foo != NULL)
+    {
+        if (foo[0] == '-')
+        {
+            if (strcmp(foo, "-l") == 0) L = 1;
+            else if (strcmp(foo, "-a") == 0) A = 1;
+            else if (strcmp(foo, "-la") == 0) A = 1, L = 1;
+            else if (strcmp(foo, "-al") == 0) A = 1, L = 1;
+        } else
+        {
+            str[index] = foo;
+            str[index][strlen(str[index])] = '\0';
+            index += 1;
+        }
+
+        foo = strtok(NULL, d);
+    }
+
+    for (int i = 0; i < index; i++)
+    {
+        char foo[path_max];
+        strcpy(foo, home);
+
+        int l = strlen(home);
+        //printf("%s\n", home);
+        if (args[1] == NULL || strcmp(args[1], "\0") == 0) chdir(home);
+        else if (args[1][0] != '~')
+        {
+            if (chdir(args[1]) != 0) perror("error (a) in cd");
+        } else if (args[1][0] == '~')
+        {
+            int i = 0;
+            for (i = 1; args[1][i] != '\0'; i++)
+                foo[i + l - 1] = args[1][i];
+            foo[i + l - 1] = '\0';
+
+            if (chdir(foo) != 0) perror("error (b) in cd");
+        } else if (chdir(args[1]) != 0) perror("error (c) in cd");
+
+    }
 }
