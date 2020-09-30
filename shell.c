b54108704a9865826cdddbd04f9c6609f469618e
banrovegrie <alapan.chaudhuri@research.iiit.ac.in>
2020-09-30 17:20:09 +0530

    made the parsings better, stress testing works

--- shell.c
+++ shell.c
@@ -13,11 +13,12 @@ char systemname[host_max], username[user_max], home[path_max], path[path_max];
 void parse(char *line, char **args, char *d)
 {
     int index = 0;
-    char *foo = strtok_r(line, d);
+    char *END;
+    char *foo = strtok_r(line, d, &END);
 
     while (foo != NULL)
     {
-        args[index] = foo, foo = strtok_r(NULL, d);
+        args[index] = foo, foo = strtok_r(NULL, d, &END);
         index += 1;
     }
     fflush(stdout);
